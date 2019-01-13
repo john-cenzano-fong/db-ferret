e2b0115f79cd5d1d54c8724f6f98b6bc91efa768
john-cenzano-fong <40726283+john-cenzano-fong@users.noreply.github.com>
2019-01-12 21:40:10 -0800

    Create xmltype.py

---
+++ xmltype.py
@@ -0,0 +1,24 @@
+import sqlalchemy.types as types
+
+# This is in here for right now to deal with more complicated postgres type
+class XMLType(types.UserDefinedType):
+    def get_col_spec(self):
+        return 'XML'
+
+    def bind_processor(self, dialect):
+        def process(value):
+            if value is not None:
+                if isinstance(value, str):
+                    return value
+                else:
+                    return etree.tostring(value)
+            else:
+                return None
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            if value is not None:
+                value = etree.fromstring(value)
+            return value
+        return process
